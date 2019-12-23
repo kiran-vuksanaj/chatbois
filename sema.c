7278751f81a3429bc42421d2347de50005d3f913
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2019-12-23 14:57:59 +0000

    theoretical semaphore wrappers

--- sema.c
+++ sema.c
@@ -19,26 +19,57 @@ union semun {
 
 int sem_config(int key,int flags,int n,...){
   int i,r,semid;
+
   semid = semget(key,n,flags);
   exit_err("configuring semaphore array",semid);
+
   va_list init_vals;
   va_start(init_vals,n);
 
   short vals[n];
   for( i=0; i<n; i++ ) vals[i] = (short)(va_arg(init_vals,int));
+
   union semun us;
   us.array = vals;
   r = semctl(semid,n,SETALL,us);
   exit_err("initializing semaphore values",r);
+
   va_end(init_vals);
   return semid;
 }
 int sem_connect(int key,int n){
-  
+  int r = semget(key,n,0);
+  exit_err("connecting to semaphores",r);
+  return r;
 }
-int sem_getval(int semid,int idx);
-void sem_disconnect(int semid);
+int sem_getval(int semid,int idx){
+  int r = semctl(semid,idx,GETVAL);
+  exit_err("checking semaphore value",r);
+  return r;
+}
+void sem_remove(int semid){
+  int r = semctl(semid,0,IPC_RMID);
+  exit_err("removing semaphore",r);
+}
 
-void sem_claim(int semid,int idx);
-void sem_release(int semid,int idx);
-void sem_listen(int semid, int idx);
+void sem_claim(int semid,int idx){
+  struct sembuf op;
+  op.sem_op = -1;
+  op.sem_num = idx;
+  int r = semop(semid,&op,1);
+  exit_err("claiming semaphore",r);
+}
+void sem_release(int semid,int idx){
+  struct sembuf op;
+  op.sem_op = 1;
+  op.sem_num = idx;
+  int r = semop(semid,&op,1);
+  exit_err("releasing semaphore",r);
+}
+void sem_listen(int semid, int idx){
+  struct sembuf op;
+  op.sem_op = 0;
+  op.sem_num = idx;
+  int r = semop(semid,&op,1);
+  exit_err("listening to semaphore",r);
+}
