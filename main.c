b96f2c6f6a7aaa9a85966b08d52db8bb64ab3548
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2019-12-23 14:43:05 +0000

    oh my god just remember permissionsgit add main.c !

--- main.c
+++ main.c
@@ -5,6 +5,6 @@
 #define KEY 24601
 
 int main(){
-  int semid = sem_config(KEY,IPC_CREAT|IPC_EXCL,4, 1,4,2,3 );
-  printf("successful config: %d",semid);
+  int semid = sem_config(KEY,IPC_CREAT|IPC_EXCL|0644,4, 1,4,2,3 );
+  printf("successful config: %d\n",semid);
 }
